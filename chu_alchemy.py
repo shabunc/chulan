a7e10098a53fb0482ead2c0ef45c21a228566322
shabunc <sh.oganjanyan>
2013-07-14 22:45:31 +0400

    added Items mapping

--- chu_alchemy.py
+++ chu_alchemy.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
+from sqlalchemy.orm import sessionmaker, relationship, backref
 import config
 
 #http://docs.sqlalchemy.org/en/rel_0_8/orm/tutorial.html
@@ -11,8 +11,10 @@ Base = declarative_base()
 
 class Projects(Base):
     __tablename__ = 'projects'
+    items = relationship("Items")
 
     name = Column(String(200), primary_key=True)
+    id = Column(Integer, primary_key=True)
 
     def __init__(self, name):
         self.name = name
@@ -20,8 +22,11 @@ class Projects(Base):
     def __repr__(self):
         return "<Project('%s')>" % (self.name)
 
+
+
 class Locales(Base):
     __tablename__ = 'locales'
+    items = relationship("Items")
     
     locale = Column(String(2), primary_key=True)
 
@@ -30,6 +35,24 @@ class Locales(Base):
 
     def __repr__(self):
         return "<Locale('%s')>" % (self.locale)
+
+
+class Items(Base):
+    __tablename__ = 'items'
+    key = Column(String(200), primary_key=True)
+    value = Column(String(400))
+    pid = Column(Integer, ForeignKey('projects.id'))
+    locale = Column(String(2), ForeignKey('locales.locale'))
+
+    def __init__(self, key, value, pid, locale):
+        self.key = key
+        self.value = value
+        self.pid = pid
+        self.locale = locale
+
+    def __repr__(self):
+        return "<Item('%s,%s,%s,%s')>" %(self.key, self.value, self.pid, self.locale)
+
 
 def bindEngine():
     conf = config.get_config()
