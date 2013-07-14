0e34df87fcc2fd4463ae8d727c01690af206e28d
shabunc <sh.oganjanyan>
2013-07-15 01:19:39 +0400

    Items with correct foreign keys constraints

--- chu_alchemy.py
+++ chu_alchemy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
+from sqlalchemy import Column, Integer, String, ForeignKey, ForeignKeyConstraint, create_engine
 from sqlalchemy.orm import sessionmaker, relationship, backref
 import config
 
@@ -11,7 +11,7 @@ Base = declarative_base()
 
 class Projects(Base):
     __tablename__ = 'projects'
-    items = relationship("Items")
+    items = relationship("Items", backref='items')
 
     name = Column(String(200), primary_key=True)
     id = Column(Integer, primary_key=True)
@@ -26,7 +26,7 @@ class Projects(Base):
 
 class Locales(Base):
     __tablename__ = 'locales'
-    items = relationship("Items")
+    items = relationship("Items", backref='items')
     
     locale = Column(String(2), primary_key=True)
 
@@ -38,20 +38,23 @@ class Locales(Base):
 
 
 class Items(Base):
+    #http://stackoverflow.com/questions/7504753/relations-on-composite-keys-using-sqlalchemy
     __tablename__ = 'items'
     key = Column(String(200), primary_key=True)
-    value = Column(String(400))
-    pid = Column(Integer, ForeignKey('projects.id'))
-    locale = Column(String(2), ForeignKey('locales.locale'))
+    value = Column(String(400), nullable=False)
+    project_id = Column(Integer)
+    project_name = Column(String(200))
+    locale_id = Column(String(2), ForeignKey('locales.locale'))
+    __table_args__ = (ForeignKeyConstraint([project_id, project_name], [Projects.id, Projects.name]), {})
 
-    def __init__(self, key, value, pid, locale):
+    def __init__(self, key, value, project_id, project_name, locale_id):
         self.key = key
         self.value = value
         self.pid = pid
         self.locale = locale
 
     def __repr__(self):
-        return "<Item('%s,%s,%s,%s')>" %(self.key, self.value, self.pid, self.locale)
+        return "<Item('%s,%s,%s,%s')>" %(self.key, self.value, self.project_name, self.locale_id)
 
 
 def bindEngine():
