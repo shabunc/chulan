652364fc309fee3b9e2eecbdbb03cf5855c2aee4
shabunc <sh.oganjanyan>
2013-07-21 22:41:36 +0400

    fixes #3 (add key CLI signature)

--- chu.py
+++ chu.py
@@ -9,7 +9,7 @@ Usage:
     chu add locale <locale>
     chu add project <project>
     chu <project> <locale> rm <key>
-    chu <project> add <key> <value> to <locale>
+    chu <project> <locale> add <key> <value> 
     chu <project> <locale> export props
     chu <project> <locale> export json
     chu <project> <locale> export xml
