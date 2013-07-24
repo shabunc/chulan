fa92d1b2042ae43e5415bde2ea1eb433fbac300a
shabunc <sh.oganjanyan>
2013-07-25 00:56:47 +0400

    adding setup.py and Manifest

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='chulan',
+    version='0.0.1',
+    author=u'Shahen Shabunc',
+    author_email='shabunc@gmail.com',
+    url='https://github.com/shabunc/chulan',
+    license='do whatever you want',
+    description='A very stupid key-valuy command line interface for i18n',
+    zip_safe=False,
+    packages=find_packages()
+)
