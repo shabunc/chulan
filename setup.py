f06bc0a0bb6e20304a15d0f6eda073d958901592
shabunc <sh.oganjanyan>
2013-07-30 02:58:06 +0400

    Fixes #11 - add --force

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='chulan',
-    version='0.0.1',
+    version='0.0.3',
     author=u'Shahen Shabunc',
     author_email='shabunc@gmail.com',
     url='https://github.com/shabunc/chulan',
